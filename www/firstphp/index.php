903509d7ddccb8d7b2d0df2f52f3d21861006910
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-06-07 22:39:34 +0300

    Page links: added some pages and linked them to the appropriete php files

--- www/firstphp/index.php
+++ www/firstphp/index.php
@@ -1,16 +1,3 @@
 <?php
-       
-$business = [
-    'name' => 'Buy codes',
-    'cost' => 200,
-    'categories' => ["Python", "PHP", "JavaScript"]
-];
 
-function register($user) {
-    //create the user record in the Database.
-    //Log them in.
-    // Send a welcome email.
-    // Reditrect them to their new dashboard.
-}
-
-Include 'index.view.php';
+require 'index.view.php';
