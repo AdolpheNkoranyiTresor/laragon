3fc486a4500a4f60db9749ec79c243b0585c6152
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-06-07 18:15:47 +0300

    Technical check-in/revision #1 (with a quiz)

--- www/firstphp/index.php
+++ www/firstphp/index.php
@@ -1,39 +1,16 @@
 <?php
        
-$allBooks = [
-    [
-
-    // In PHP, the correct syntax for array keys is the => character (equals sign followed by greater-than sign).
-
-        'title' => 'Lion and the Mouse',
-        'author' => 'Jerry Pinkney',
-        'publishedYear' => 2009,
-        'purchaseUrl' => 'https://en.wikipedia.org/wiki/The_Lion_%26_the_Mouse'
-    ],
-    [
-        'title' => 'Are You My Mother?',
-        'author' => 'P. D. Eastman',
-        'publishedYear' => 1960,
-        'purchaseUrl' => 'https://www.goodreads.com/book/show/197084.Are_You_My_Mother_'
-    ],
-    [
-        'title' => 'Project Hail Mary',
-        'author' => 'Andy Weir',
-        'publishedYear' => 2021,
-        'purchaseUrl' => 'https://www.amazon.com/Project-Hail-Mary-Andy-Weir/dp/0593135202'  
-    ],
-    [
-        'title' => 'The Martian',
-        'author' => 'Andy Weir',
-        'publishedYear' => 2011,
-        'purchaseUrl' => 'https://www.amazon.com/Martian-Andy-Weir/dp/0553418025'  
-    ],
+$business = [
+    'name' => 'Buy codes',
+    'cost' => 200,
+    'categories' => ["Python", "PHP", "JavaScript"]
 ];
 
-// Use a PHP duiltin function called array_filter
+function register($user) {
+    //create the user record in the Database.
+    //Log them in.
+    // Send a welcome email.
+    // Reditrect them to their new dashboard.
+}
 
-$filteredBooks = array_filter($allBooks, function ($book){
-    return $book['publishedYear'] >= 1950 && $book['publishedYear'] <= 2020 ;
-});
-
-require 'index.view.php';
+Include 'index.view.php';
--- www/firstphp/index.view.php
+++ www/firstphp/index.view.php
@@ -8,27 +8,20 @@
     <style>
         body{
             display: grid;
-            /* text-align: center; */
             place-items: center;
-            /* height: 100vh; */
             margin: 0;
             font-family: arial;
         }
     </style>
 </head>
-<body>
-    <div>    
-        <h1>List of books filtered by author and the released/publised year:</h1>
+<body>   
+    <h1><?= $business['name'] ?></h1>
 
-        <ul>
-            <?php foreach ($filteredBooks as $book) : ?>
-                <li>
-                    <?= $book['title']; ?>
-                    <?= ' ' . 'was published in' . ' ' . $book['publishedYear'] . ' ' . 'and written by' . ' ' . $book['author'] . '.'; ?>
-                </li>
-            <?php endforeach; ?> 
-        </ul> 
-    </div> 
+    <ul>
+        <?php foreach ($business['categories'] as $category) :?>
+            <li><?= $category; ?></li>
+        <?php endforeach; ?>
+    </ul>
     
 </body>
 </html>
