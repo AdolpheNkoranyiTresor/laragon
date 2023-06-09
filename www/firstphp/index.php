82992f5b05e6827dfd8f78a889913846735bab1b
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-06-09 13:02:01 +0300

    Added a dump and die function on the index.php file/Home page

--- www/firstphp/index.php
+++ www/firstphp/index.php
@@ -2,4 +2,22 @@
 
 $heading = "Home";
 
+//dump and die
+// echo "<pre>";
+// var_dump($_SERVER);
+// echo "</pre>";
+// die();
+
+// This function work similarly as the above dump code
+
+function dd($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+dd($_SERVER);
+
 require 'views/index.view.php';
