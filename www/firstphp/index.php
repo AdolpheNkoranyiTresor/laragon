cb1491ddf67812adbcd23d89e1317c3b0d011462
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-06-17 11:50:16 +0300

    Added the routes var with controllers, and created an abort function.

--- www/firstphp/index.php
+++ www/firstphp/index.php
@@ -4,12 +4,26 @@ require 'functions.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-if ($uri === '/') {
-    require 'controllers/index.php';
-} else if ($uri === '/about') {
-    require 'controllers/about.php';
-} else if ($uri === '/contact') {
-    require 'controllers/contact.php';
-}else if ($uri === '/ourmission') {
-    require 'controllers/ourmission.php';
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+    '/ourmission' => 'controllers/ourmission.php',
+];
+
+
+if (array_key_exists($uri, $routes)){
+    require $routes[$uri];
+} else {
+    abort();
 }
+
+function abort($code = 404) {
+    http_response_code($code);
+
+    require "views/{$code}.php";
+
+    die();
+}
+
+routeToController($uri, $routes);
---
+++ www/kilo/index.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo 'hello Michael bla bla!';
+
+?>
