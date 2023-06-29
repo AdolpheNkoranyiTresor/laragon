595fe6ecf513a9652052c53f5a23667a524a5bff
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-06-30 00:29:29 +0300

    Home work: Created a prepared statement to fetch the data in test that has an id of 1. Then, experimented with calling fetch() instead of fetchAll()

--- www/firstphp/index.php
+++ www/firstphp/index.php
@@ -6,14 +6,14 @@ require 'functions.php';
 
 //connect to our MySQL database
 
+// Created a prepared statement to fetch the data in test that has an id of 1. Then, experimented with calling fetch() instead of fetchAll()
+
 $dsn = "mysql:host=localhost;port=3306;user=root;dbname=firstphp;charset=utf8mb4";
 $pdo = new PDO($dsn);
 
-$statement = $pdo->prepare("SELECT * From test;");
+$statement = $pdo->prepare("SELECT * From test WHERE id = 1");
 $statement->execute();
 
-$test = $statement->fetchAll(PDO::FETCH_ASSOC );
+$test = $statement->fetch(PDO::FETCH_ASSOC);
 
-foreach ($test as $result){
-    echo "<li>" . $result['user_name'] . " " . $result['email'] . "</li>";
-}
+    echo "<li>" . $test['user_name'] . " " . $test['email'] . "</li>";
