30662045c901356292c18a49fbdd89fe7b17dec5
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-07-03 12:28:56 +0300

    Created the first instense of a class, prepare a query to send to MySQL Database

--- www/firstphp/index.php
+++ www/firstphp/index.php
@@ -6,14 +6,14 @@ require 'functions.php';
 
 //connect to our MySQL database
 
-// Created a prepared statement to fetch the data in test that has an id of 1. Then, experimented with calling fetch() instead of fetchAll()
-
 $dsn = "mysql:host=localhost;port=3306;user=root;dbname=firstphp;charset=utf8mb4";
 $pdo = new PDO($dsn);
 
-$statement = $pdo->prepare("SELECT * From test WHERE id = 1");
+$statement = $pdo->prepare("SELECT * From test;");
 $statement->execute();
 
-$test = $statement->fetch(PDO::FETCH_ASSOC);
+$test = $statement->fetchAll(PDO::FETCH_ASSOC );
 
-    echo "<li>" . $test['user_name'] . " " . $test['email'] . "</li>";
+foreach ($test as $result){
+    echo "<li>" . $result['user_name'] . " " . $result['email'] . "</li>";
+}
