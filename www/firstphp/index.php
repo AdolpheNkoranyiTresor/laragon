a908700927aaa8e90e0a5b9ffe49751064539d91
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-07-04 00:22:01 +0300

    Extracting a dedicated Database class, and Created a Database file

--- www/firstphp/index.php
+++ www/firstphp/index.php
@@ -4,25 +4,9 @@ require 'functions.php';
 
 // require 'router.php';
 
-//connect to the database, execute a query.
-
-class Database {
-    public function query($query)
-    {
-        $dsn = "mysql:host=localhost;port=3306;user=root;dbname=firstphp;charset=utf8mb4";
-        $pdo = new PDO($dsn);
-
-        $statement = $pdo->prepare($query);
-        $statement->execute();
-
-        return $statement->fetchAll(PDO::FETCH_ASSOC );
-    }
-}
+include 'Database.php';
 
 $db = new Database();
-$test = $db->query("SELECT * From test;");
+$test = $db->query("SELECT * From test;")->fetchALL(PDO::FETCH_ASSOC);
 
-
-foreach ($test as $result){
-    echo "<li>" . $result['user_name'] . " " . $result['email'] . "</li>";
-}
+dd($test);
