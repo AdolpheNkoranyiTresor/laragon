8597bb820dcf32a170905d1a68b2b459f86286db
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-07-03 23:25:07 +0300

    connect to the database, execute a query. Trigger a cicle query

--- www/firstphp/index.php
+++ www/firstphp/index.php
@@ -4,15 +4,24 @@ require 'functions.php';
 
 // require 'router.php';
 
-//connect to our MySQL database
+//connect to the database, execute a query.
 
-$dsn = "mysql:host=localhost;port=3306;user=root;dbname=firstphp;charset=utf8mb4";
-$pdo = new PDO($dsn);
+class Database {
+    public function query()
+    {
+        $dsn = "mysql:host=localhost;port=3306;user=root;dbname=firstphp;charset=utf8mb4";
+        $pdo = new PDO($dsn);
 
-$statement = $pdo->prepare("SELECT * From test;");
-$statement->execute();
+        $statement = $pdo->prepare("SELECT * From test;");
+        $statement->execute();
 
-$test = $statement->fetchAll(PDO::FETCH_ASSOC );
+        return $statement->fetchAll(PDO::FETCH_ASSOC );
+    }
+}
+
+$db = new Database();
+$test = $db->query();
+
 
 foreach ($test as $result){
     echo "<li>" . $result['user_name'] . " " . $result['email'] . "</li>";
