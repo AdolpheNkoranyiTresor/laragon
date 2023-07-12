bb3b96539ad7c036a7c7e59cc7320ce8690f22fb
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-07-12 13:27:50 +0300

    Persist a new note to the database. Created a new security concern that requires us to always escape user-provided input by adding an *htmlspecialchars* code averywhere we display the note.

--- www/firstphp/controllers/note-create.php
+++ www/firstphp/controllers/note-create.php
@@ -1,9 +1,16 @@
 <?php
 
-$heading = "New note";
+$config = include('config.php');
+
+$db = new Database($config['database']);
+
+$heading = "Create note";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    dd($_POST);
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);
 }
 
 require 'views/note-create.view.php';
