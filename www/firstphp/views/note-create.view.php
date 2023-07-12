2ced957df401737c49a6bfbc54289fa566f5375c
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-07-13 00:54:30 +0300

    Apply a layer validation so that a user cannot insert an empty data into our database table

--- www/firstphp/views/note-create.view.php
+++ www/firstphp/views/note-create.view.php
@@ -4,32 +4,32 @@
 
 
 <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 md:grid-cols-3 mt-5">
-        
-        <form method="POST">
-            <div class="space-y-3 shadow sm:overflow-hidden sm:rounded-md p-5">
-                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
-                        <div class="mt-2">
-                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                            <textarea name="body" id="body" rows="3" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Here is an idea for note ..."></textarea>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="sm:col-span-4">
-                        <div class="mt-1">
-                            <div class="bg-gray-30 px-4 py-3 text-right sm:px-6">
-                            <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 text-white p-1">Submit</button>
-                            </div>
-                        </div>
-                    </div>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <div class="md:grid md:grid-cols-3 md:gap-6">
+            <div class="mt-5 md:col-span-2 md:mt-0">
+                <form method="POST">
+                    <div class="shadow sm:overflow-hidden sm:rounded-md">
+                        <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
+                            <div>
+                                <label for="body" class="block text-sm font-medium text-gray-700">Body</label>
+                                <div class="mt-1">
+                                    <textarea id="body" name="body" rows="4" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Here's an idea for a note..."
+                                    ><?= $_POST['body'] ?? '' ?></textarea>
 
-                </div>
+                                    <?php if (isset($errors['body'])) : ?>
+                                        <p class="text-green-700 text-xs mt-2"><?= $errors['body'] ?></p>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                            <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Submit</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-
-        </form>
-
+        </div>
     </div>
 </main>
     
