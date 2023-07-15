bbd6c927d9b9c60415faedcca3164dec60713a0b
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-07-15 12:44:58 +0300

    Changed the directory of the function && router files, and updated the require statement with base_path.

--- www/firstphp/Core/router.php
+++ www/firstphp/Core/router.php
@@ -1,11 +1,11 @@
 <?php
 
-$routes = require('routes.php');
+$routes = require base_path('routes.php');
 
 
 Function routerToController($uri, $routes){
     if (array_key_exists($uri, $routes)){
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     } else {
         abort();
     }
@@ -14,7 +14,7 @@ Function routerToController($uri, $routes){
 function abort($code = 404) {
     http_response_code($code);
 
-    require "views/{$code}.php";
+    require base_path("views/{$code}.php");
 
     die();
 }
